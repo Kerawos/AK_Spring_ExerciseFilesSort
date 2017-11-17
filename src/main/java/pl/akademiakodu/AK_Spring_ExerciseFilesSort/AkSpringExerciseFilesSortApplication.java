3a100c4718a8ee6e73b95145c3bf570d0dc163f0
Kerawos <Maresowa@gmail.com>
2017-11-17 14:26:32 +0100

    Initial commit

---
+++ src/main/java/pl/akademiakodu/AK_Spring_ExerciseFilesSort/AkSpringExerciseFilesSortApplication.java
@@ -0,0 +1,12 @@
+package pl.akademiakodu.AK_Spring_ExerciseFilesSort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkSpringExerciseFilesSortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkSpringExerciseFilesSortApplication.class, args);
+	}
+}
